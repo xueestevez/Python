8b86e11c5938b1d66922861cc6c1f9470f0fc12f
xueestevez <8g5p24pkph@privaterelay.appleid.com>
2026-05-27 12:30:59 -0700

    this is a conditional script

---
+++ conditionals.py
@@ -0,0 +1,7 @@
+#this is a conditionals script
+bananas = int(input("Enter the number of bananas you have: "))
+if bananas > 5:
+    print("I have a lot of bananas!")
+elif bananas < 5:
+    print("I don't have enough bananas.")
+
