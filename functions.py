b3fd2ea3e78f36a522d4d239e6088284bea947aa
xueestevez <8g5p24pkph@privaterelay.appleid.com>
2026-05-28 09:03:46 -0700

    making it a fruitful function

--- functions.py
+++ functions.py
@@ -1,4 +1,7 @@
 #this is a set of functions
-def user_info(name, lastname, age):
-    return f"Your name is {name} {lastname} and you are {age} years old."
-print(user_info("John", "Doe", 30))
+def user_info(x, y, age):
+    return f"Your name is {x} {y} and you are {age} years old."
+x = input("Enter your first name: ")
+y = input("Enter your last name: ")
+z = int(input("Enter your age: "))
+print(user_info(x, y, z))
