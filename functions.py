9b2cba828dbf05be51626ee506bda7d4214c266f
xueestevez <8g5p24pkph@privaterelay.appleid.com>
2026-05-28 09:21:04 -0700

    refined the function definition by removing the type hint and added a specific name for the last name input.

--- functions.py
+++ functions.py
@@ -1,6 +1,6 @@
 #this is a set of functions
-def user_info(x, y, age):
-    return f"Your name is {x} {y} and you are {age} years old."
+def user_info(x, y, z):
+    return f"Your name is {x} {y} and you are {z} years old."
 x = input("Enter your first name: ")
 y = input("Enter your last name: ")
 z = int(input("Enter your age: "))
